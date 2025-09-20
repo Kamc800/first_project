3524f1ccf624f4fe778de4982eaa25a33ce60986
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2025-09-20 19:05:58 +0100

    add steps to push to github

--- patrick.py
+++ patrick.py
@@ -1,2 +1,12 @@
 name = "Patrick"
 print(name)
+
+# Always use comments to explain your code
+# Also push to github regularly
+
+# codes to push to github:
+# git add (file name>) or git add . (to add all files)
+# git commit -m "your message"
+# git push
+# git status (to check the status of your files)
+# git log (to check the history of your commits)
