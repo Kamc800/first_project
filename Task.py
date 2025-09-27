a5bca2715ab594f3c61afaac867f4dafe537bcf2
Kamc800 <Kamsyu14@gmail.com>
2025-09-27 15:32:35 +0100

    Arithmatic Operation

---
+++ Task.py
@@ -0,0 +1,16 @@
+age = 11
+height = 5.8
+# Addition
+print(age + height)
+# Subtraction
+print(age - height)
+# Multiplication
+print(age * height)
+# Division
+print(age / height)
+# Modulo
+print(age % height)
+# Floor Division
+print(age // height)
+# Exponetation
+print(age ** height)
