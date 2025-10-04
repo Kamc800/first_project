41845c7e74c09d0bc94967a0d9d25321f46c6a7c
Kamc800 <Kamsyu14@gmail.com>
2025-10-04 15:48:42 +0100

    I made a calculator for your country

---
+++ age_of_a_country.py
@@ -0,0 +1,3 @@
+country = input('Please enter the name of a country: ')
+independence = int(input('Please enter the year the country gained independence: '))
+print(country, 'is', 2025 - independence, 'years old')
