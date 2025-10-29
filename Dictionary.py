ca6a73ffe6cddfaf5a0f0af3a178d2a27cbead7a
Kamc800 <Kamsyu14@gmail.com>
2025-10-29 18:09:34 +0000

    I fixed the bugs

--- Dictionary.py
+++ Dictionary.py
@@ -1,7 +1,8 @@
 contact_book = {
-    "Kamsi": "0989474367"
-    "Oscar": "0927789377"
-    "Layton": "0987654321"
-    "Richard": "0987865432"
+    "Kamsi": "0989474367",
+    "Oscar": "0927789377",
+    "Layton": "0987654321",
+    "Richard": "0987865432",
 }
 print(contact_book)
+print(contact_book["Kamsi"])
