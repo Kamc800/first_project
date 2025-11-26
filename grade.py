890fef4a9e53a9fe642b8e9f7dc0c7b51096fee6
Kamc800 <Kamsyu14@gmail.com>
2025-11-26 17:59:49 +0000

    The work grounds for the latest part of builtin.py

---
+++ grade.py
@@ -0,0 +1,14 @@
+def check_grade(score):
+    return check_grade
+print("Hello pls enter your score")
+score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score <= 89:
+    print("B")
+elif score >= 70 and score <= 79:
+    print("C")
+elif score >= 60 and score <= 69:
+    print("D")
+else:
+    print("F")
