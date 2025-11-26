aa2ef634245a982281a9b49f4f27841209c48f82
Kamc800 <Kamsyu14@gmail.com>
2025-11-26 17:50:40 +0000

    i refined user defined functions and made a grade checker

--- Builtin.py
+++ Builtin.py
@@ -61,3 +61,18 @@ check_even(number)
 def find_largest(a, b, c):
     return max(a, b, c)
 print(find_largest(6, 7, 8))
+
+def check_grade(score):
+    return check_grade
+print("Hello pls enter your score")
+score = int(input("Score: "))
+if score >= 90 and score <= 100:
+    print("A")
+elif score >= 80 and score <= 89:
+    print("B")
+elif score >= 70 and score <= 79:
+    print("C")
+elif score >= 60 and score <= 69:
+    print("D")
+else:
+    print("F")
